f179a9b13008306b3bf645cd020b7b61f6e06c57
Isaac Kosmos <isaackosmos@gmail.com>
2025-09-04 09:31:09 -0300

    feat: auth context in navigator

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react";
+import { useCallback } from "react";
 
 import { SystemBars } from "react-native-edge-to-edge";
 import { NavigationContainer } from "@react-navigation/native";
@@ -6,11 +6,13 @@ import { NavigationContainer } from "@react-navigation/native";
 import { PublicRoutes } from "./PublicRoutes";
 import { PrivateRoutes } from "./PrivateRoutes";
 
+import { useAuthContext } from "@/context/auth.context";
+
 const NavigationRoutes = () => {
-  const [user, setUser] = useState(undefined);
+  const { user, token } = useAuthContext();
 
   const Routes = useCallback(() => {
-    return user ? <PrivateRoutes /> : <PublicRoutes />;
+    return user && token ? <PrivateRoutes /> : <PublicRoutes />;
   }, []);
 
   return (
