c99b672d0f2c3539e5740d1f81a4e42494eefabf
Isaac Kosmos <isaackosmos@gmail.com>
2025-09-23 09:55:43 -0300

    feat: new transaction inputs

--- src/components/NewTransaction/index.tsx
+++ src/components/NewTransaction/index.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 
 import { CreateTransactionInterface } from "@/shared/interfaces/https/create-transaction-request";
 import { colors } from "@/shared/colors";
 import { useBottomSheetContext } from "@/context/bottomSheet.context";
+import CurrencyInput from "react-native-currency-input";
 
 export const NewTransaction = () => {
   const { closeBottomSheet } = useBottomSheetContext();
@@ -16,6 +17,13 @@ export const NewTransaction = () => {
     value: 0,
   });
 
+  const setTransactionData = (
+    key: keyof CreateTransactionInterface,
+    value: string | number
+  ) => {
+    setTransaction((prev) => ({ ...prev, [key]: value }));
+  };
+
   return (
     <View className="px-8 py-5">
       <TouchableOpacity
@@ -26,6 +34,27 @@ export const NewTransaction = () => {
 
         <MaterialIcons name="close" color={colors.gray["700"]} size={20} />
       </TouchableOpacity>
+
+      <View className="flex-1 mt-8 mb-8">
+        <TextInput
+          onChangeText={(text) => setTransactionData("description", text)}
+          placeholder="Descrição"
+          placeholderTextColor={colors.gray["700"]}
+          value={transaction.description}
+          className="text-white text-lg h-[50px] bg-background-primary my-2 rounded-[6] pl-4"
+        />
+
+        <CurrencyInput
+          value={transaction.value}
+          delimiter="."
+          separator=","
+          precision={2}
+          minValue={0}
+          onChangeValue={(value) => setTransactionData("value", value ?? 0)}
+          prefix="R$ "
+          className="text-white text-lg h-[50px] bg-background-primary my-2 rounded-[6] pl-4"
+        />
+      </View>
     </View>
   );
 };
